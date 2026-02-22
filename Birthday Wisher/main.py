d6ff95bc58a9c508bfde3b691bd200537d813b4d
Ahmed Alawlaqi <ahmed.alawlaqi25@gmail.com>
2026-02-23 01:59:36 +0300

    Update main.py

--- Birthday Wisher/main.py
+++ Birthday Wisher/main.py
@@ -3,25 +3,21 @@ import pandas as pd
 import random
 import datetime as dt
 import smtplib
-# 1. Update the birthdays.csv
 
 birth_data = pd.read_csv("birthdays.csv")
 new_data = birth_data.to_dict()
 name = new_data['name'][0]
 email = new_data['email'][0]
 
-# 2. Check if today matches a birthday in the birthdays.csv
 year = new_data['year'][0]
 month = new_data['month'][0]
 day = new_data['day'][0]
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 with open(f"letter_templates/letter_{random.randint(1,3)}.txt") as letter:
     new_name = (letter.readline().replace('[NAME]', name))
     next_line = letter.readlines()
     final_letter = new_name + "".join(next_line)
 
-# 4. Send the letter generated in step 3 to that person's email address.
 birth_day = dt.datetime(year,month,day)
 current_time = dt.datetime.now()
 current_date = current_time.date()
